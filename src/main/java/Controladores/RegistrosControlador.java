b7455506d6dd6140293928de8716af16daa9523f
Emilio-Camino <adrian.camino@epn.edu.ec>
2025-02-11 17:12:09 -0500

    Busqueda de datos en la tabla de Registros Implementacion de un to.String() de la multa para obtener su costo Modificacion en los if/else de revision para motor y frenos, para evitar ambiguedades (en la secuencia if y else if se usaba dos veces el operador >= o <=). Se modifico Revision.fxml para que diga "Sistema de Frenos" en lugar "Frenos" en la seccion Datos Vehiculo II

--- src/main/java/Controladores/RegistrosControlador.java
+++ src/main/java/Controladores/RegistrosControlador.java
@@ -1,12 +1,20 @@
 package Controladores;
 
 import LogicaRevision.Revision;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.collections.transformation.FilteredList;
+import javafx.collections.transformation.SortedList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
@@ -14,30 +22,106 @@ import java.io.EOFException;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
+import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.ResourceBundle;
 
-public class RegistrosControlador {
+public class RegistrosControlador implements Initializable {
 
     // ARRAY LIST DE TODAS LAS REVISIONES HECHAS A LO LARGO DEL PROGRAMA
     ArrayList<Revision> revisiones = new ArrayList<>();
     @FXML
-    private TextField buscar;
+    private TextField buscarField;
 
-    @FXML
-    private Button cargarResultados;
     @FXML
     private Button menuPrincipalBoton;
 
+    //TableView
     @FXML
-    void cargarArchivo(ActionEvent event) throws IOException, ClassNotFoundException {
-        leerRevisiones();
-        for(Revision revision : revisiones) {
-            System.out.println(revision);
+    private TableColumn<Revision, String> placaColumna;
+
+    @FXML
+    private TableColumn<Revision, String> propietarioColumna;
+    @FXML
+    private TableColumn<Revision, String> aproboColumna;
+    @FXML
+    private TableColumn<Revision, String> observacionesColumna;
+    @FXML
+    private TableColumn<Revision, String> multaColumna;
+
+    @FXML
+    private TableView<Revision> tablaRevisiones;
+
+    //Lista observable para guardar la informacion
+    private final ObservableList<Revision> observableRevisiones = FXCollections.observableArrayList();
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        // Set the cell value factories using lambdas to access nested properties
+        placaColumna.setCellValueFactory(cellData ->
+                new SimpleStringProperty(cellData.getValue().getRevisionPropietario().getVehiculoPropietario().getPlaca())
+        );
+
+        propietarioColumna.setCellValueFactory(cellData ->
+                new SimpleStringProperty(cellData.getValue().getRevisionPropietario().getIdPropietario())
+        );
+
+        aproboColumna.setCellValueFactory(cellData ->
+                new SimpleStringProperty(cellData.getValue().getNumeroErrores() > 0 ? "NO APROBADO" : "APROBADO")
+        );
+
+        observacionesColumna.setCellValueFactory(cellData ->
+                new SimpleStringProperty(cellData.getValue().getObservaciones().toString())
+        );
+
+        multaColumna.setCellValueFactory(cellData -> {
+            Revision revision = cellData.getValue();
+            String multaText = (revision.getNumeroErrores() > 0 && revision.getRevisionPropietario().getMultaNoPasar() != null)
+                    ? revision.getRevisionPropietario().getMultaNoPasar().mostrarCostoMulta()
+                    : "SIN MULTA";
+            return new SimpleStringProperty(multaText);
+        });
+
+        // Load data from file
+        try {
+            leerRevisiones();
+        } catch (IOException e) {
+            System.err.println("Error al leer el archivo");
+        } catch (ClassNotFoundException e) {
+            System.err.println("Clase no hallada");
         }
-        buscar.setDisable(false);
+
+        // Populate observable list
+        observableRevisiones.addAll(revisiones);
+
+        // Set items in TableView
+        tablaRevisiones.setItems(observableRevisiones);
+
+        //Creando una FilteredList para englobar a la lista observable
+        FilteredList<Revision> filteredData = new FilteredList<>(observableRevisiones, b -> true);
+        buscarField.textProperty().addListener((observable, oldValue, newValue) -> {
+            filteredData.setPredicate(revision -> {
+                if (newValue == null || newValue.isEmpty()) {
+                    return true;
+                }
+                String placaBusqueda = newValue.toUpperCase();
+                if(revision.getRevisionPropietario().getVehiculoPropietario().getPlaca().toUpperCase().indexOf(placaBusqueda) != -1) {
+                    return true;
+                } else {
+                    return false;
+                }
+            });
+        });
+        //Englobando toda la informacion en una lista ordenada
+        SortedList<Revision> datosOrdenados = new SortedList<>(filteredData);
+        //Ordenandoel TableView
+        datosOrdenados.comparatorProperty().bind(tablaRevisiones.comparatorProperty());
+        tablaRevisiones.setItems(datosOrdenados);
+
+
     }
 
     @FXML
